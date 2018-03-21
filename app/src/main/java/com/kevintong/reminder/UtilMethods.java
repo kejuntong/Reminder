0a9e033cd8caedd31dd45fa52a0aa6dd7e2dfa9b
Kevin Tong <kejuntong@gmail.com>
2018-03-20 22:31:23 -0400

    expanded view task title frame

--- app/src/main/java/com/kevintong/reminder/UtilMethods.java
+++ app/src/main/java/com/kevintong/reminder/UtilMethods.java
@@ -1,5 +1,8 @@
 package com.kevintong.reminder;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
+
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -12,6 +15,16 @@ public class UtilMethods {
     public static String timeToString(long timeStamp){
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm aaa @ EEE, MMM d, yyyy", Locale.US);
         return sdf.format(timeStamp);
+    }
+
+    public static int dpToPx (Context context, int dp){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public static int pxToDp(Context context, int px) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return Math.round(px / (displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
 }
--- app/src/main/java/com/kevintong/reminder/activities/HomeActivity.java
+++ app/src/main/java/com/kevintong/reminder/activities/HomeActivity.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.RelativeLayout;
 
 import com.kevintong.reminder.CallbackInterface;
 import com.kevintong.reminder.Constants;
 import com.kevintong.reminder.MyApp;
 import com.kevintong.reminder.R;
+import com.kevintong.reminder.UtilMethods;
 import com.kevintong.reminder.adapters.TaskAdapter;
 import com.kevintong.reminder.database.TaskDbUtilMethods;
 import com.kevintong.reminder.models.TaskDetails;
@@ -24,6 +30,7 @@ import com.kevintong.reminder.database.TaskDbHelper;
 import com.kevintong.reminder.models.TaskName;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class HomeActivity extends Activity {
 
@@ -31,6 +38,10 @@ public class HomeActivity extends Activity {
     TaskAdapter taskAdapter;
     ArrayList<TaskName> taskList;
     ArrayList<ArrayList<TaskDetails>> taskDetailList;
+
+    RelativeLayout selectionFrame;
+
+    private View lastExpandedView = null;
 
     Button addButton;
 
@@ -44,6 +55,11 @@ public class HomeActivity extends Activity {
         setAddButton();
 
         loadDataFromDb();
+
+        initSelectionFrame();
+
+        setSelectionAnimation();
+
     }
 
     @Override
@@ -93,20 +109,17 @@ public class HomeActivity extends Activity {
         });
     }
 
-    public void initListView(){
-
+    private void initListView(){
         taskList = new ArrayList<>();
         taskDetailList = new ArrayList<>();
         expandableListView = (ExpandableListView) findViewById(R.id.list_view);
         taskAdapter = new TaskAdapter(HomeActivity.this, taskList, taskDetailList);
-
         taskAdapter.setRemoveItemCallback(new CallbackInterface() {
             @Override
             public void onCallback(Object object) {
                 loadDataFromDb();
             }
         });
-
         expandableListView.setAdapter(taskAdapter);
     }
 
@@ -146,6 +159,77 @@ public class HomeActivity extends Activity {
         cursor.close();
         readDb.close();
         taskAdapter.notifyDataSetChanged();
+    }
+
+    private void initSelectionFrame(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        selectionFrame = (RelativeLayout) inflater.inflate(R.layout.view_black_frame, null, false);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int screenWidth = displayMetrics.widthPixels;
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                screenWidth - UtilMethods.dpToPx(this,10),
+                UtilMethods.dpToPx(this, 50));
+        params.setMarginStart(UtilMethods.dpToPx(this,5));
+        selectionFrame.setLayoutParams(params);
+
+        RelativeLayout rootView = (RelativeLayout) findViewById(R.id.activity_home);
+        rootView.addView(selectionFrame);
+    }
+
+    private void setSelectionAnimation(){
+
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, final View view, int position, long id) {
+                selectionFrame.setVisibility(View.VISIBLE);
+                if (!expandableListView.isGroupExpanded(position)){
+                    Log.d("TAG", "kejun, view height: " + UtilMethods.pxToDp(HomeActivity.this, view.getHeight() ));
+
+                    expandableListView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            float yPosition = view.getY();
+                            selectionFrame.setY(yPosition + UtilMethods.dpToPx(HomeActivity.this, 5));
+                        }
+                    });
+
+                    lastExpandedView = view;
+                }
+
+                else {
+                    if (lastExpandedView != view){
+                        expandableListView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                float yPosition = lastExpandedView.getY();
+                                selectionFrame.setY(yPosition + UtilMethods.dpToPx(HomeActivity.this, 5));
+                            }
+                        });
+                    } else {
+
+
+                        // TODO:
+                        selectionFrame.setVisibility(View.GONE);
+                    }
+
+                }
+
+
+                return false;
+            }
+        });
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int position) {
+
+            }
+        });
+
+
     }
 
 }
